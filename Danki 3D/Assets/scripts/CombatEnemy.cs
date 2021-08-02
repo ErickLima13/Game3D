b2c5adb5b44ff59211670f25a0fac3d124649cb8
Erick Lima <eriquipe7@gmail.com>
2021-08-02 10:51:42 -0300

    initial

---
+++ Danki 3D/Assets/scripts/CombatEnemy.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CombatEnemy : MonoBehaviour
+{
+    [Header("Atributtes")]
+    public float totalHealth = 100;
+    public float attackDamage;
+    public float movementSpeed;
+    public float lookRadius;
+    public float colliderRadius = 2f;
+    public float rotationSpeed;
+
+    [Header("Components")]
+    private Animator anim;
+    private CapsuleCollider capsule;
+    private NavMeshAgent agent;
+
+    [Header("Others")]
+    private Transform player;
+
+    private bool walking;
+    private bool attacking;
+    private bool hiting;
+
+    private bool waitFor;
+    private bool playerIsDead;
+
+    [Header("WayPoints")]
+    public List<Transform> wayPoints = new List<Transform>();
+    public int currentPathIndex;
+    public float pathDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        capsule = GetComponent<CapsuleCollider>();
+        agent = GetComponent<NavMeshAgent>();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (totalHealth > 0)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+
+            if (distance <= lookRadius)
+            {
+                // o personagem está no raio de ação
+                agent.isStopped = false;
+                if (!attacking)
+                {
+                    agent.SetDestination(player.position);
+                    anim.SetBool("Walk Forward", true);
+                    walking = true;
+                }
+
+
+
+                if (distance <= agent.stoppingDistance)
+                {
+                    //o perosnagem está no raio de ataque
+                    //aqui vem o metodo de ataque
+                    StartCoroutine("Attack");
+                    LookTarget();
+
+                }
+                else
+                {
+                    attacking = false;
+                }
+            }
+            else
+            {
+                //o personagem está fora do raio de ação
+                //agent.isStopped = true;
+                anim.SetBool("Walk Forward", false);
+                walking = false;
+                attacking = false;
+                MoveToWayPoint();
+            }
+        }
+    }
+
+    void MoveToWayPoint()
+    {
+        if (wayPoints.Count > 0)
+        {
+            float distance = Vector3.Distance(wayPoints[currentPathIndex].position, transform.position);
+            agent.destination = wayPoints[currentPathIndex].position;
+
+
+            if(distance <= pathDistance)
+            {
+                //parte para o proximo ponto
+                currentPathIndex = Random.Range(0, wayPoints.Count);
+
+            }
+
+            anim.SetBool("Walk Forward", true);
+            walking = true;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if (!waitFor && !hiting && !playerIsDead)
+        {
+            waitFor = true;
+            attacking = true;
+            walking = false;
+            anim.SetBool("Walk Forward", false);
+            anim.SetBool("Attack 01", true);
+            yield return new WaitForSeconds(1.2f);
+            GetPlayer();
+            //yield return new WaitForSeconds(1f);
+            waitFor = false;
+        }
+
+        if (playerIsDead)
+        {
+            anim.SetBool("Walk Forward", false);
+            anim.SetBool("Attack 01", false);
+            walking = false;
+            attacking = false;
+            agent.isStopped = true;
+        }
+    }
+
+    void GetPlayer()
+    {
+        
+        foreach(Collider c in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+                //aplicar dano no player
+                c.gameObject.GetComponent<Player>().GetHit(attackDamage);
+                playerIsDead = c.gameObject.GetComponent<Player>().isDead;
+
+            }
+        }
+    }
+
+    public void GetHit(float damage)
+    {
+        totalHealth -= damage;
+
+        if(totalHealth > 0)
+        {
+            //inimigo ainda está vivo
+            StopCoroutine("Attack");
+            anim.SetTrigger("Take Damage");
+            hiting = true;
+            StartCoroutine("recoveryFromHit");
+
+        }
+        else
+        {
+            //inimigo morre
+            anim.SetTrigger("Die");
+            //Destroy(gameObject);
+        }
+    }
+
+    IEnumerator recoveryFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("Walk Forward", false);
+        anim.SetBool("Attack 01", false);
+        hiting = false;
+        waitFor = false;
+    }
+
+    void LookTarget()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+
+}
---
+++ Danki 3D/Assets/scripts/Player.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    private CharacterController controller;
+
+    [Header("Atributtes")]
+    public float totalHealth;
+    public float speed;
+    public float gravity;
+    public float damage = 20;
+
+    private Animator anim;
+
+    private Transform cam;
+
+    Vector3 moveDirection;
+
+    private bool isWalking;
+    private bool waitFor;
+    private bool isHitting;
+    public bool isDead;
+
+
+    public float smoothRotTime;
+    private float turnSmoothVelocity;
+
+    public float colliderRadius;
+    public List<Transform> enemyList = new List<Transform>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            Move();
+            GetMouseInput();
+        }
+        
+        
+    }
+
+    //metodo da movimentação do personagem
+    void Move()
+    {
+        if (controller.isGrounded)
+        {
+            //pega o input horizontal (teclas direita/esquerda)
+            float horizontal = Input.GetAxisRaw("Horizontal");
+
+            //pega o input vertical (teclas cima/baixo)
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            //variavel local que armzazena os eixos horizontal e vertical
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+            //verifica se o personagem está movimentando (caso o input seja maior que zero)
+            if (direction.magnitude > 0)
+            {
+                if (!anim.GetBool("attacking"))
+                {
+                    //variavel local que armazena a rotação e o angulo de vizualização da camera 
+                    float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+                    //variavel local que armazena a rotacao porem mais suave
+                    float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+
+                    //passamos a rotação suave ao personagem
+                    transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+                    //armazena a direção com base na direção do mouse
+                    moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+
+                    //chama a animação walk
+                    anim.SetInteger("transition", 1);
+
+                    isWalking = true;
+                }
+                else
+                {
+                    anim.SetBool("walking", false);
+                    moveDirection = Vector3.zero;
+                }
+            }
+            //freia o personagem
+            else if(isWalking)
+            {
+                // é executado quando o personagem está parado
+                anim.SetBool("walking", false);
+
+                //chama a animação iddle
+                anim.SetInteger("transition", 0);
+                moveDirection = Vector3.zero;
+
+                isWalking = false;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //move o personagem
+        controller.Move(moveDirection  * Time.deltaTime);
+
+    }
+
+    void GetMouseInput()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (anim.GetBool("walking"))
+                {
+                    anim.SetBool("walking", false);
+                    anim.SetInteger("transition", 0);
+                }
+
+                if (!anim.GetBool("walking"))
+                {
+                    StartCoroutine("Attack");
+                }
+
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if (!waitFor && !isHitting)
+        {
+            waitFor = true;
+            anim.SetBool("attacking", true);
+            anim.SetInteger("transition", 2);
+
+            yield return new WaitForSeconds(0.4f);
+
+            GetEnemiesList();
+
+            foreach (Transform e in enemyList)
+            {
+                //aplica dano ao inimigo
+                CombatEnemy enemy = e.GetComponent<CombatEnemy>();
+
+                if (enemy != null)
+                {
+                    enemy.GetHit(damage);
+                    
+                }
+            }
+
+            yield return new WaitForSeconds(1f);
+
+            anim.SetInteger("transition", 0);
+            anim.SetBool("attacking", false);
+            waitFor = false;
+        }
+    }
+
+    void GetEnemiesList()
+    {
+        enemyList.Clear();
+        foreach (Collider c in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (c.gameObject.CompareTag("Enemy"))
+            {
+                enemyList.Add(c.transform);
+            }
+        }
+    }
+
+    public void GetHit(float damage)
+    {
+        totalHealth -= damage;
+
+        if(totalHealth > 0)
+        {
+            //player ainda está vivo
+            StopCoroutine("Attack");
+            anim.SetInteger("transition", 3);
+            isHitting = true;
+            StartCoroutine("RecoveryFromHit");
+        }
+        else
+        {
+            //player morre
+            isDead = true;
+            anim.SetTrigger("die");
+        }
+    }
+
+    IEnumerator RecoveryFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetInteger("transition", 0);
+        isHitting = false;
+        anim.SetBool("attacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + transform.forward, colliderRadius);
+    }
+
+}
